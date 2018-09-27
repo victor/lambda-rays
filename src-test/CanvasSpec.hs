76bcb082e54c03762f3e9431dfab1792d1885a52
Victor Jalencas <victor.jalencas@xing.com>
2018-09-27 12:58:32 +0200

    Color tests

--- src-test/CanvasSpec.hs
+++ src-test/CanvasSpec.hs
@@ -1,7 +1,6 @@
 module CanvasSpec where
 
 import Test.Tasty.Hspec
-
 import Canvas
 
 spec_canvas :: Spec
@@ -15,3 +14,40 @@ spec_canvas = do
             green c `shouldBe` 0.4
         it "c.blue == 1.7" $
             blue c `shouldBe` 1.7
+
+    describe "Color Operations" $ do
+        describe "Adding colors" $ do
+            let c1 = Color (0.9, 0.6, 0.75)
+            let c2 = Color (0.7, 0.1, 0.25)
+            it "c1 + c2 == Color (1.6, 0.7, 1.0)" $
+                (c1 + c2) ~= Color (1.6, 0.7, 1.0)
+        describe "Subtracting colors" $ do
+            let c1 = Color (0.9, 0.6, 0.75)
+            let c2 = Color (0.7, 0.1, 0.25)
+            it "c1 - c2 == Color (0.2, 0.5, 0.5)" $
+                (c1 - c2) ~= Color (0.2, 0.5, 0.5)
+        describe "Multiplying a color by a scalar" $ do
+            let c = Color (0.2, 0.3, 0.4)
+            it "2 *| c == Color (0.4, 0.6, 0.8)" $
+              (2 *| c) ~= Color (0.4, 0.6, 0.8)
+        describe "Multiplying colors" $ do
+            let c1 = Color(1, 0.2, 0.4)
+            let c2 = Color (0.9, 1, 0.1)
+            it "c1 ⨯ c2 == Color (0.9, 0.2, 0.04)" $
+              c1 ⨯ c2 ~= Color (0.9, 0.2, 0.04)
+
+
+-- This should be factored out, probably
+-- For now keeping it close to edit it more conveniently
+class ApproxEq a where
+    (~=) :: a -> a -> Bool
+    (/~=) :: a -> a -> Bool
+
+instance ApproxEq Float where
+    x ~= x' = abs (x-x') < epsilon
+        where epsilon = 0.000001 -- good enough for Floats
+    x /~= x' = not $ x ~= x'
+
+instance ApproxEq Color where
+    Color (r, g, b) ~= Color (r', g', b') = r ~= r' && g ~= g' && b ~= b'
+    x /~= x' = not $ x ~= x'
