90bc56e7179ed89d67c5a02ade417d5e1ef65c62
Victor Jalencas <victor.jalencas@xing.com>
2018-09-27 12:59:14 +0200

    Color tests implemented

--- src/Canvas.hs
+++ src/Canvas.hs
@@ -3,10 +3,12 @@ module Canvas (
     , red
     , green
     , blue
+    , (*|)
+    , (⨯)
 ) where
 
 -- No point in generalizing the type, Float is more than enough for all the possible colors
-data Color = Color (Float, Float, Float) deriving (Show)
+data Color = Color (Float, Float, Float) deriving (Eq, Show)
 
 first :: (Float, Float, Float) -> Float
 first (t1, _, _) = t1
@@ -26,3 +28,18 @@ green (Color t) = second t
 
 blue :: Color -> Float
 blue (Color t) = third t
+
+(*|) :: Float -> Color -> Color
+a *| Color (r1, g1, b1) = Color (a * r1, a * g1, a * b1)
+
+
+instance Num Color where
+    Color (r1, g1, b1) + Color (r2, g2, b2) = Color (r1 + r2, g1 + g2, b1 + b2)
+    _ * _ = undefined
+    abs (_) = undefined
+    signum (_) = undefined
+    fromInteger _ = undefined
+    negate (Color (r, g, b)) = Color ((-r), (-g), (-b))
+
+(⨯) :: Color -> Color -> Color
+Color (r1, g1, b1) ⨯ Color (r2, g2, b2) = Color (r1 * r2, g1 * g2, b1 * b2) -- Hadamard product
