98e33e276673d9bc64a4661984fe80ea1d4a987b
Victor Jalencas <victor.jalencas@xing.com>
2019-12-05 10:52:30 +0100

    WIP

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -1,6 +1,33 @@
 {-# OPTIONS_GHC -fno-warn-type-defaults #-}
 
+import Projectile
 import Quadruple
+import Canvas
+
+loopWorld :: World -> Projectile -> [Projectile]
+loopWorld wo p' = do
+    let p = tick wo p'
+    if y (position p) <= 0 then [p]
+    else p : loopWorld wo p
+
 
 main :: IO ()
-main = print $ show $ Quadruple 0 0 0 0
+main = do
+    let width = 900
+    let height = 550
+
+    let start = point 0 1 0
+    let vel = normalize(vector 1  1.8  0) |* 11.25
+
+    let p = Projectile { position = start, velocity = vel}
+    let g = vector 0 (-0.1) 0
+    let wi = vector (-0.01) 0 0
+    let world = World {gravity = g, wind = wi} 
+    
+
+    let canvass = foldl (\pixel c -> writePixelAt fst pixel snd pixel c Color(0.8, 0.8, 0.1))  createCanvas width height  [ ((floor (x pos)), (floor (height - (y pos)))) | i <- loopWorld world p, pos <- position i]
+
+
+    print ppmFromCanvas canvass
+
+
--- src/Quadruple.hs
+++ src/Quadruple.hs
@@ -27,7 +27,7 @@ toList :: (Floating a) => Quadruple a -> [a]
 toList Quadruple { x = x1,  y = y1, z = z1, w = w1 } = [x1, y1, z1, w1]
 
 fromList :: (Floating a) => [a] -> Quadruple a
-fromList [a, b, c, d] = (Quadruple {x = a, y = b, z = c, w = d})
+fromList [a, b, c, d] = Quadruple {x = a, y = b, z = c, w = d}
 fromList _ = undefined
 
 point :: (Floating a) => a -> a -> a -> Quadruple a
@@ -55,25 +55,25 @@ isVector q = w q == 0.0
 
 
 instance Floating a => Num (Quadruple a) where
-  q1 + q2 = fromList (zipWith (+) (toList (q1)) (toList (q2)))
-  Quadruple _ _ _ _  * Quadruple _ _ _ _ = undefined
-  abs (Quadruple _ _ _ _) = undefined
-  signum (Quadruple _ _ _ _) = undefined
+  q1 + q2 = fromList (zipWith (+) (toList q1) (toList q2))
+  Quadruple {}  * Quadruple {} = undefined
+  abs _ = undefined
+  signum _ = undefined
   fromInteger _ = undefined
   negate a = qmap a negate
 
 
 magnitude :: (Floating a) => Quadruple a -> a
-magnitude q = sqrt ((x q)**2 + (y q)**2 + (z q)**2)
+magnitude q = sqrt (x q ** 2 + y q ** 2 + z q ** 2)
 
 normalize :: (Floating a) => Quadruple a -> Quadruple a
 normalize q = qmap q ( / magnitude q)
 
 dot :: (Floating a) => Quadruple a -> Quadruple a -> a
-dot a b = ((x a) * (x b)) +
-  ((y a) * (y b)) +
-  ((z a) * (z b)) +
-  ((w a) * (w b))
+dot a b = (x a * x b) +
+  (y a * y b) +
+  (z a * z b) +
+  (w a * w b)
 
 (⨯) :: (Floating a) => Quadruple a -> Quadruple a -> Quadruple a
-(⨯) a b = vector (((y a)*(z b))-((z a)*(y b))) (((z a)*(x b))-((x a)*(z b))) (((x a)*(y b))-((y a)*(x b)))
+(⨯) a b = vector ((y a * z b)-(z a * y b)) ((z a * x b)-(x a * z b)) ((x a * y b)-(y a * x b))
